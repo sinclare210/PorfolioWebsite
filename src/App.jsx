472acf0e877319d7d6b92c9c9d24bd09b506f27f
Sinclair <sinclairolajuwon@gmail.com>
2024-10-16 19:40:53 -0500

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
 import Navbar from "./components/Navbar"
+import Intro from "./components/Intro"
 
 export default function App() {
   return (
     <div className="w-full h-screen overflow-x-hidden">
     <Navbar/>
+    <Intro/>
     </div>
   )
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const Intro = () => {
+  return (
+    <div className="flex flex-col-reverse md:flex-row items-center  text-center md:text-left px-4  text-white space-y-8 md:space-y-0 md:space-x-8 mt-28  md:justify-center absolute">
+      
+      {/* Text Section */}
+      <div className="md:w-1/2 mt-5">
+        <p className="text-lg">Hello,</p>
+        <h1 className="text-4xl font-bold mt-4">
+          I'm <span className="text-yellow-500">Sinclair</span>, Frontend Developer
+        </h1>
+        <p className="mt-4 text-lg">
+          As a frontend developer, I focus on crafting intuitive, 
+          visually appealing, and responsive user interfaces 
+          that deliver seamless interactions across various 
+          devices and screen sizes, ensuring that the experience 
+          is not only functional but also engaging and accessible 
+          for all users. To achieve this, I rely on a strong foundation 
+          in HTML, CSS, and JavaScript, utilize modern frameworks like 
+          React to build dynamic components, and prioritize performance 
+          optimization, accessibility, and design aesthetics. Collaborating 
+          with designers and backend teams, I ensure smooth API integrations, 
+          state management, and debugging to create polished, efficient web applications.
+        </p>
+        <Button className="bg-yellow-400 text-black text-base font-bold rounded-lg px-5 py-2 hover:bg-yellow-300 transition duration-200 my-4">
+              Contact Me
+            </Button>
+      </div>
+      
+      {/* Image Section */}
+      <div className="md:w-1/4">
+        <img 
+          src="/mypic.jpeg" 
+          alt="Sinclair" 
+          className="rounded-lg shadow-lg w-96 object-cover h-96"
+        />
+      </div>
+      
+    </div>
+  )
+}
+
+export default Intro
