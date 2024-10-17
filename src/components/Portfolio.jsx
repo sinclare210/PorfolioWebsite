a6f64b15a290c89e454378aa2e12de4a1eab1e6e
Sinclair <sinclairolajuwon@gmail.com>
2024-10-17 16:31:22 -0500

    about portfolio done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,16 +14,16 @@ const Portfolio = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6'>
                 <a href='https://todo-list-project-mrzp.vercel.app/' target='_blank' rel='noopener noreferrer' className="block transform hover:scale-105 transition-transform duration-300">
-                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto h-96 md:h-full" src="public/screencapture-todo-list-project-mrzp-vercel-app-2024-09-19-09_34_55.png" alt="To-do List Project" />
+                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto" src="public/screencapture-todo-list-project-mrzp-vercel-app-2024-09-19-09_34_55.png" alt="To-do List Project" />
                 </a>
                 <a href='https://meme-generator-two-ochre.vercel.app/' target='_blank' rel='noopener noreferrer' className="block transform hover:scale-105 transition-transform duration-300">
-                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto h-96 md:h-full" src="public/screencapture-localhost-5173-2024-09-30-05_10_53.png" alt="Meme Generator" />
+                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto" src="public/screencapture-localhost-5173-2024-09-30-05_10_53.png" alt="Meme Generator" />
                 </a>
                 <a href='https://sinc-view.vercel.app/' target='_blank' rel='noopener noreferrer' className="block transform hover:scale-105 transition-transform duration-300">
-                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto h-96 md:h-full" src="public/screencapture-sinc-view-vercel-app-2024-10-17-05_39_32.png" alt="Sinc View" />
+                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto" src="public/screencapture-sinc-view-vercel-app-2024-10-17-05_39_32.png" alt="Sinc View" />
                 </a>
                 <a href='https://quiz-app-ten-green-22.vercel.app/' target='_blank' rel='noopener noreferrer' className="block transform hover:scale-105 transition-transform duration-300">
-                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto h-96 md:h-full" src="public/screencapture-quiz-app-ten-green-22-vercel-app-2024-10-17-05_34_30.png" alt="Quiz App" />
+                    <img className="rounded-lg shadow-lg w-full md:w-3/4 mx-auto" src="public/screencapture-quiz-app-ten-green-22-vercel-app-2024-10-17-05_34_30.png" alt="Quiz App" />
                 </a>
             </div>
         </div>
